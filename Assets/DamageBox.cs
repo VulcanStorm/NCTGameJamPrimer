50cff443b8e8308ee0acf67aac77438ddc442e10
VulcanStorm <vulcanstorm98@gmail.com>
2015-11-16 09:14:58 +0000

    health and respawns
    Added the stuff from health and respawns

---
+++ Assets/DamageBox.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DamageBox : MonoBehaviour {
+	
+	public int damage = 20;
+	
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+	
+	void OnCollisionEnter (Collision col){
+		
+		// check if we have collided with the player
+		if(col.collider.tag == "Player"){
+			
+			print ("player collided");
+			// send the player a message saying it took damage
+			col.transform.SendMessage("TakeDamage",damage);
+			// destroy this box
+			Destroy (this.gameObject);
+		}
+		
+	}
+}
+
+
+
+
+
+
+
+
+
+
+
---
+++ Assets/Health_And_Respawns/PlayerHealth.cs
@@ -0,0 +1,73 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerHealth : MonoBehaviour {
+	
+	public int currentHealth = 100;
+	
+	public Vector3 spawnPosition = Vector3.zero;
+	
+	public bool autoSetup = true;
+	
+	// Use this for initialization
+	void Start () {
+		
+		// check if we need to auto setup
+		if(autoSetup == true){
+			spawnPosition = transform.position;
+		}
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+		// check if health <= 0
+		if(currentHealth <= 0){
+			// player dead
+			print ("player dead");
+			// respawn the player
+			RespawnPlayer ();
+		}
+		
+	}
+	
+	
+	public void RespawnPlayer () {
+		
+		// set our objects position to the spawn position
+		transform.position = spawnPosition;
+		
+		// reset our health
+		currentHealth = 100;
+		
+	}
+	
+	
+	public void TakeDamage (int damage){
+		
+		// take damage away from health
+		currentHealth = currentHealth - damage;
+		
+	}
+	
+	public void HealHealth (int healing){
+		
+		// add healing to current health
+		currentHealth = currentHealth + healing;
+		
+	}
+	
+}
+
+
+
+
+
+
+
+
+
+
+
+
