5278a16d3a8746ff4a092e7a83fc1b3ca0127320
VulcanStorm <vulcanstorm98@gmail.com>
2016-02-11 19:07:14 +0000

    Saving Data
    added saving and loading data example

---
+++ Assets/SavingData/PlayerData.cs
@@ -0,0 +1,64 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.IO;
+using System;
+using System.Xml;
+using System.Xml.Serialization;
+
+[System.Serializable]
+public struct PlayerData {
+
+	public string playerName;
+	public int playerHealth;
+	public float xPos;
+	public float yPos;
+	public float zPos;
+	
+	public static bool DoSaveData (string filePath, PlayerData dataToSave) {
+		
+		try{
+			// create the xml serialiser
+			XmlSerializer xmlSerialiser = new XmlSerializer(typeof(PlayerData));
+			// create a file stream
+			// using statement: to ensure that the file is closed if an error occurs
+			using(FileStream fileStream = new FileStream(filePath,FileMode.Create)){
+				// now serialise the data
+				xmlSerialiser.Serialize(fileStream,dataToSave);
+				// now close the file
+				fileStream.Close();
+			}
+		}
+		
+		catch(Exception e){
+			Debug.LogException(e);
+			return false;
+		}
+		return true;
+		
+	}
+	
+	public static PlayerData DoLoadData (string filePath){
+		
+		PlayerData returnData = new PlayerData();
+		
+		try{
+			// create xml serialiser
+			XmlSerializer xmlSerialiser = new XmlSerializer(typeof(PlayerData));
+			// create file stream
+			using(FileStream fileStream = new FileStream(filePath, FileMode.Open)){
+				// get the return data
+				returnData = (PlayerData)xmlSerialiser.Deserialize(fileStream);
+				// close the file
+				fileStream.Close ();
+			}
+			
+		}
+		catch (Exception e){
+			Debug.LogException(e);
+			
+		}
+		
+		return returnData;
+	}
+	
+}
---
+++ Assets/SavingData/PlayerSaveScript.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerSaveScript : MonoBehaviour {
+	
+	public PlayerData myPlayerData = new PlayerData();
+	public string saveFilePath;
+	
+	
+	// Use this for initialization
+	void Start () {
+		saveFilePath = Application.dataPath + "/testsave.sav";
+	}
+	
+	void OnGUI () {
+		if(GUI.Button(new Rect(0,0,100,25),"Save")){
+			PlayerData.DoSaveData(saveFilePath,myPlayerData);
+		}
+		
+		if(GUI.Button(new Rect(0,30,100,25),"Load")){
+			myPlayerData = PlayerData.DoLoadData(saveFilePath);
+		}
+	}
+}
